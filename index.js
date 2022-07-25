5a611e4fe37041e606ee54d081f481812ded8d6a
nazeh <ar.nazeh@gmail.com>
2022-07-25 11:00:29 +0300

    remove unused method _makeBee

--- index.js
+++ index.js
@@ -94,15 +94,6 @@ module.exports = class HyperBundle extends EventEmitter {
     return this._closing
   }
 
-  _makeBee (key) {
-    const metadataOpts = key
-      ? { key, cache: true, onwait: this._onwait }
-      : { name: 'db', cache: true, onwait: this._onwait }
-    const core = this.corestore.get(metadataOpts)
-    const metadata = { contentFeed: null }
-    return new Hyperbee(core, { keyEncoding: 'utf-8', valueEncoding: 'json', metadata })
-  }
-
   async _close () {
     if (this._batching) return this.files.close()
 
